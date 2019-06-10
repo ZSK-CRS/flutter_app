4105fa77a1a077e25127de8f2e4fabecc6ac8120
ZSK-CRS <1625127317@qq.com>
2019-06-10 18:54:05 +0800

    基本布局

---
+++ lib/flutter_layout_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//如何进行Flutter的布局开发
+class FlutterLayoutPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _FlutterLayoutPageState();
+  }
+}
+
+class _FlutterLayoutPageState extends State<FlutterLayoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
